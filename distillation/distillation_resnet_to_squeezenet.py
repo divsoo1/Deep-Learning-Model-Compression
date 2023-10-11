668e171e07e19a984b38eef455522feb5f47069a
DIvyam Sood <divyamsood.sood@gmail.com>
2023-10-11 18:22:39 +0200

    refactoring

--- distillation/distillation_resnet_to_squeezenet.py
+++ distillation/distillation_resnet_to_squeezenet.py
@@ -8,28 +8,27 @@ from ..utils import paths
 if __name__ == "__main__":
 
     init_seed()
+    batch_size = 256
+    lr = 0.001
+    teacher_model_name = "resnet50"
+    tacher_model_checkpoints = paths.RESNET50_FULL_TRAINING
+    temperature = 1
+    alpha = 0.5
+    num_classes = 525
+    distillation_epochs = 30
+    final_checkpoint_dir = paths.SQUEEZENET_DISTILLATION_CHECKPOINTDIR
 
-    BATCH_SIZE = 256
-    LR = 0.001
-    TEACHER_MODEL_NAME = "resnet50"
-    TEACHER_MODEL_CHECKPOINTS = paths.RESNET50_FULL_TRAINING
-    TEMPERATURE = 1
-    ALPHA = 0.5
-    NUM_CLASSES = 525
-    NUM_DISTILLATION_EPOCHS = 30
-    FINAL_CHECKPOINT_DIR = paths.SQUEEZENET_DISTILLATION_CHECKPOINTDIR
+    train_loader, val_loader = create_data_loaders(paths.TRAIN_DIR, paths.VALID_DIR, batch_size)
+    teacher_model = load_model(teacher_model_name, tacher_model_checkpoints, num_classes=num_classes, only_last=True, pretrained=True)
+    student_model = create_model("squeeze_net", num_classes=num_classes, only_last=False, pretrained=False)
 
-    train_loader, val_loader = create_data_loaders(paths.TRAIN_DIR, paths.VALID_DIR, BATCH_SIZE)
-    teacher_model = load_model(TEACHER_MODEL_NAME, TEACHER_MODEL_CHECKPOINTS, num_classes=NUM_CLASSES, only_last=True, pretrained=True)
-    student_model = create_model("squeeze_net", num_classes=NUM_CLASSES, only_last=False, pretrained=False)
-
-    optimizer = optim.Adam(student_model.parameters(), lr=LR)
+    optimizer = optim.Adam(student_model.parameters(), lr=lr)
     distilled_model = train_and_evaluate_kd(
         student_model, teacher_model, optimizer, loss_kd, train_loader, val_loader, 
-        temperature=TEMPERATURE, alpha=ALPHA, num_epochs=NUM_DISTILLATION_EPOCHS, save_path=FINAL_CHECKPOINT_DIR
+        temperature=temperature, alpha=alpha, num_epochs=distillation_epochs, save_path=final_checkpoint_dir
     )
 
-    save_model(distilled_model, checkpoint_dir=FINAL_CHECKPOINT_DIR)
+    save_model(distilled_model, checkpoint_dir=final_checkpoint_dir)
 
 
 
